ec4115ad634dad5bc1b1298519c062a17dc8624f
alessandraschiller <42187050+alessandraschiller@users.noreply.github.com>
2018-11-14 20:14:25 -0500

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,14 +1,34 @@
-CREATE DATABASE Bamazon_DB;
-USE Bamazon_DB;
+CREATE DATABASE bamazon;
+USE bamazon;
 
 CREATE TABLE products (
-	item_id INT(11) AUTO_INCREMENT NOT NULL,
-	product_name VARCHAR(30) NOT NULL,
-	department_name VARCHAR(20) NOT NULL,
+	item_id INTEGER AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(45) NOT NULL,
+	department_name VARCHAR(45) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
-	stock_quanity INT(11) NOT NULL,
+	stock_quanity INTEGER(10) NOT NULL,
 	PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quanity)
-VALUES ('')
+VALUES ('Narwhal Onesie', 'Home', '69.50', '389'),
+	('Gordon Ramsay\'s Autobiography', 'Books', '13.01', '453'),
+	('A Massive Parachute', 'Fun Things', '25.99', '100'),
+	('Bellatrix Lestrange\'s Wand', 'Fun Things', '52.97', '47'),
+	('Charles Darwin ornament', 'Fun Things', '12.61', '207'),
+	('Flaming Unicorn Mousepad', 'Home', '7.99', '532'),
+	('Red Chili Pepper Neck Pillow', 'Home', '8.99', '455'),
+	('Bubble Wrap Calendar', 'Home', '26.99', '734'),
+	('Sea Monkey Kit', 'Pet', '10.50', '789'),
+	('Zac Efron Cake Toppers', 'Home', '3.25', '896'),
+	('Batman-themed Bathroom Set', 'Home', '122.31', '49'),
+	('Pet Hawaiian Shirt', 'Pet', '10.99', '313'),
+	('Adult Coloring Book', 'Books', '8.99', '245'),
+	('When You Find Out the World Is Against You: And Other Funny Memories About Awful Moments', 'Books', '10.99', '320'),
+	('The World According to Rick (A Rick And Morty Book) By Rick Sanchez', 'Books', '20.00', '234'),
+	('Prosecco Made Me Do It', 'Books', '12.99', '178'),
+	('Dog Lion Mane', 'Pet', '15.99', '37'),
+	('Gingerbread Dog Costume', 'Pet', '22.85', '109');
+
+	SELECT * FROM bamazon.products;
+    
